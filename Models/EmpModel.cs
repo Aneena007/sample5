04808e4ce5b4b8e694854ee723dd2bb70a339676
Aneena Shaji <ashaji@suyati.com>
2022-03-11 14:55:51 +0530

    first commit

---
+++ Models/EmpModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CRUDUsingMVC.Models
+{
+    public class EmpModel
+    {
+        [Display(Name ="id")]
+        public int Empid { get; set; }
+
+        [Required(ErrorMessage ="First name is Required.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage ="city is required.")]
+        public string City { get; set; }
+
+        [Required(ErrorMessage ="Address is required")]
+        public string Address { get; set; }
+
+    }
+}
